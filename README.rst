3153ba200160633ae8c19652885c52028e42e32e
Michael Stewart <statueofmike@gmail.com>
2018-05-25 15:21:38 -0500

    updated README
    prepared for PyPI publish

---
+++ README.rst
@@ -0,0 +1,21 @@
+
+=============
+ elf Package
+=============
+
+Parse Executable-Linking Files (Unix ELF binary files). See https://en.wikipedia.org/wiki/Executable_and_Linkable_Format for general reference.
+
+Script Usage
+
+    elf.py <input ELF filename>
+
+==========
+ Features
+==========
+
+* 32- and 64-bit ELF parsing
+* ELF class for convenient abstraction
+* ElfBytes class for byte-level operations
+* Summary inspection
+* Dynamic linking dependency lookup
+
--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup
+from os import path
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+version = '1.0.0'
 
 setup(name='unix-elf'
-    , version='1.0.0'
+    , version=version
     , description='An ELF binary parser'
+    , long_description=long_description
+    , author = 'Michael Stewart'
+    , author_email = 'statueofmike@gmail.com'
     , url='https://github.com/statueofmike/Elf-Parser'
+    , download_url="https://github.com/statueofmike/Elf-Parser/archive/{0}.tar.gz".format(version)
     , license='MIT'
-    , packages=['elf']
+    , packages=['unix-elf']
+    , classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Disassemblers',
+      ]
+    , keywords='elf binary'
     , install_requires=[]
     , python_requires='>=2.6'
     )
